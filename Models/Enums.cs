44509dd3c2109b56eca47754b9c142f401427993
Your Name <amostafa.ghonem@gmail.com>
2026-04-28 15:41:07 +0300

    Setup code

---
+++ Models/Enums.cs
@@ -0,0 +1,26 @@
+namespace SchoolManagementSystem.Models
+{
+    // enums 3shan both nst5dmha fe Student w Instructor
+    public enum Faculty
+    {
+        CS,
+        Engineering,
+        Arts,
+        Business,
+        Science
+    }
+
+    public enum StudentStatus
+    {
+        Active,
+        Graduated,
+        Withdrawn
+    }
+
+    public enum InstructorStatus
+    {
+        FullTime,
+        PartTime
+        
+    }
+}
